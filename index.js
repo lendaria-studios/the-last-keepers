447a546add2aab34c99e83b30cdb0582756aab0f
Cami1409 <camilla.s2004@aluno.ifsc.edu.br>
2024-10-02 18:16:14 +0000

    criar jogo mínimo com Phaser #10

---
+++ index.js
@@ -0,0 +1,10 @@
+import config from './config.js'
+class Game extends Phaser.Game {
+    constructor () {
+        super(config)  
+    }
+}
+
+window.onload = () => {
+    window.game = new Game()
+}
