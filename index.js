7de588009a1c991cda6e7795118ed723eb00ebd4
Cami1409 <camilla.s2004@aluno.ifsc.edu.br>
2024-10-09 17:34:55 +0000

    criar cenas do jogo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import config from './config.js'
-class Game extends Phaser.Game {
-    constructor () {
-        super(config)  
-    }
-}
+import abertura from './abertura.js'
+import sala from './sala.js'
 
+class Game extends Phaser.Game {
+  constructor () {
+    super(config)
+    this.scene.add('abertura', abertura)
+    this.scene.add('sala', sala)
+    this.scene.start('abertura')
+  }
+}
 window.onload = () => {
-    window.game = new Game()
+  window.game = new Game()
 }
