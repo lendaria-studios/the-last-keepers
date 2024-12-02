781ad7eb34ee6e2220d30e4ed46ee50baeb37d51
amanda <amanda.c28@aluno.ifsc.edu.br>
2024-12-02 17:37:48 +0000

    logos

---
+++ cliente/sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'cache-v1'
+const precacheResources = [
+    './',
+    './index.html',
+    'capa.png',
+]
+
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!')
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)))
+})
+
+self.addEventListener('activate', (event) => {
+    console.log('Service worker activate event!')
+})
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request)
+        })
+    )
+})
